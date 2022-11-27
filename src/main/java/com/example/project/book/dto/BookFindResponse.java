5dc1e5ae352b8ab894a18a5b7492d6b24de471a7
ksh103 <jayy_z@naver.com>
2022-11-28 03:43:39 +0900

    chore : DTO 일부 수정

--- src/main/java/com/example/project/book/dto/BookFindResponse.java
+++ src/main/java/com/example/project/book/dto/BookFindResponse.java
@@ -1,29 +1,20 @@
 package com.example.project.book.dto;
 
 import com.example.project.book.domain.Book;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@NoArgsConstructor
+@AllArgsConstructor
 public class BookFindResponse {
 
     private Long bookId;
     private String bookName;
     private String authorName;
     private String publisherName;
-
-    private BookFindResponse(final Long bookId,
-                             final String bookName,
-                             final String authorName,
-                             final String publisherName) {
-        this.bookId = bookId;
-        this.bookName = bookName;
-        this.authorName = authorName;
-        this.publisherName = publisherName;
-    }
 
     public static BookFindResponse from (final Book book) {
         return BookFindResponse.builder()
